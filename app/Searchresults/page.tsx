f91b7556f6d3a8ecf6fde0a7376a30aa85c53b44
aryann <36785654+aryann2018@users.noreply.github.com>
2023-10-27 13:06:02 +0530

    Update page.tsx

--- app/Searchresults/page.tsx
+++ app/Searchresults/page.tsx
@@ -3,28 +3,14 @@ import React from 'react';
 import { motion } from 'framer-motion'; // Import motion from framer-motion
 import FilterPagination from '../Home/FilterPagination';
 import SubmitCaseForReview from '@/Components/SubmitCaseForReview';
-
-import Example from '@/Components/navigation/Example'
-//import HeadNav from '@/Components/HeaderNav';
-//import { fadeIn } from './variants';
-//import { motion } from "framer-motion"
-
-
 const Searchresults = () => {
   return (
     <>
-	 
-
-	 <Example />
-      <section
-        id="searchresults"
-        className="mb-1"
-      >
+      <section id="searchresults" className="mb-1">
         <FilterPagination />
         <SubmitCaseForReview />
       </section>
     </>
   );
 }
-
 export default Searchresults;
